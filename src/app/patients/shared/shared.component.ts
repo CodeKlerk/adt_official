dedda2d8720ab71cf450e7efd6a445e478a36cc4
bryan-mwas <mwathibrian7@gmail.com>
2017-02-08 23:42:16 +0300

    Reactive Patient Form. Default today dates set

--- src/app/patients/shared/shared.component.ts
+++ src/app/patients/shared/shared.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, DoCheck, ViewChild, AfterViewChecked, OnChanges } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { PatientsService } from '../patients.service';
 import { Patient, Service, Status, Regimen, Prophylaxis, Who_stage, Source, Illness, Allergies, FamilyPlanning, Locations } from '../patients';
@@ -10,7 +11,8 @@ declare var $: any;
 
 @Component({
     selector: 'patient-form',
-    templateUrl: './shared-form.component.html'
+    templateUrl: './shared-form.component.html',
+    providers: [DatePipe]
 })
 
 // TODO: Handle error catching in all subscriptions
@@ -18,7 +20,10 @@ declare var $: any;
 export class SharedComponent implements OnInit, DoCheck, OnChanges {
 
     @Input() formType: string;
+    // Determine the controls to be displayed.
     form: string;
+    patientForm: FormGroup;
+    today = new Date();
 
     // Define properties first.
     @Input() patient = new Patient;
@@ -36,6 +41,11 @@ export class SharedComponent implements OnInit, DoCheck, OnChanges {
     patientProphylaxis: Observable<IMultiSelectOption[]>;
     familyPlanning: Observable<FamilyPlanning[]>;
     birth_place: Observable<Locations[]>;
+    // Variables to multiselect controls
+    family_planning_list: string[];
+    chronic_illness_list: string[];
+    allergies_list: string[];
+    prophylaxis_list: string[];
 
     private selectedOptions: string[]; // Default selection
 
@@ -66,7 +76,12 @@ export class SharedComponent implements OnInit, DoCheck, OnChanges {
     };
 
     // Methods section: The constructor comes first!
-    constructor(private _patientService: PatientsService, private router: Router) { }
+    constructor(
+        private _datePipe: DatePipe,
+        private router: Router,
+        private _patientService: PatientsService,
+        private fb: FormBuilder,
+    ) { }
 
     ngOnInit(): void {
         this.birth_place = this._patientService.getLocation();
@@ -78,111 +93,140 @@ export class SharedComponent implements OnInit, DoCheck, OnChanges {
         this.patientRegimen = this._patientService.getRegimen();
         this.patientWhostage = this._patientService.getWho_stage();
         this.patientProphylaxis = this._patientService.getProphylaxis();
+        this.patientForm = this.fb.group({
+            id: ['', Validators.required],
+            ccc_number: ['', Validators.required],
+            first_name: ['', Validators.required],
+            last_name: ['', Validators.required],
+            other_name: ['', Validators.required],
+            birth_date: ['', Validators.required],
+            place_of_birth: ['', Validators.required],
+            age_in_years: ['', Validators.required],
+            age_in_months: ['', Validators.required],
+            current_weight: ['', Validators.required],
+            current_height: ['', Validators.required],
+            bsa: ['', Validators.required],
+            phone_number: ['', Validators.required, Validators.maxLength(10)],
+            physical_address: ['', Validators.required],
+            gender: ['', Validators.required],
+            is_pregnant: ['', Validators.required],
+            is_tb: ['', Validators.required],
+            is_tb_tested: ['', Validators.required],
+            tb_start: [''],
+            tb_end: [''],
+            is_sms: ['', Validators.required],
+            is_smoke: ['', Validators.required],
+            is_alcohol: ['', Validators.required],
+            current_status: ['', Validators.required],
+            enrollment_date: [this.nextAppointment(), Validators.required],
+            regimen_start_date: [this.nextAppointment(), Validators.required],
+            regimen_id: ['', Validators.required],
+            service_id: ['', Validators.required],
+            facility_id: ['', Validators.required],
+            supporter_id: ['', Validators.required],
+            who_stage_id: ['', Validators.required],
+            prophylaxis: ['', Validators.required],
+            source_id: ['', Validators.required],
+            status: ['', Validators.required],
+            disclosure: ['', Validators.required],
+            spouse_ccc: ['', Validators.required],
+            patient_status: '',
+            family_planning: ['', Validators.required],
+            support_group: [{ value: '', disabled: true }, Validators.required],
+            alternate_contact: ['', Validators.required],
+            other_drugs: [{ value: '', disabled: true }, Validators.required],
+            other_illness: [{ value: '', disabled: true }, Validators.required],
+            other_allergies: [{ value: '', disabled: true }, Validators.required],
+            illnesses: ['', Validators.required],
+            drug_allergies: ['', Validators.required],
+            tb_category: ['', Validators.required],
+            tb_phase: ['', Validators.required],
+            start_tb_phase: ['', Validators.required],
+            end_tb_phase: ['', Validators.required],
+            pep_reason: ['', Validators.required],
+            isoniazid_start: ['', Validators.required],
+            isoniazid_end: ['', Validators.required],
+            is_support: '',
+            is_illness: '',
+            is_drugs: '',
+            is_allergies: ''
+        });
     }
+
+     nextAppointment(): string {
+    let date = new Date();
+    return this._datePipe.transform(date, 'MM/dd/y'); // using angular's built in date pipe to format date object.
+  }
+
     ngDoCheck(): void {
-        this.patient.bsa = Math.sqrt((this.patient.current_weight * this.patient.current_height) / 3600);
+        // Dynamically sets the multiselect values to the form.
+        console.log(this.chronic_illness_list)
+        this.patientForm.patchValue({
+            family_planning: this.family_planning_list,
+            illnesses: this.chronic_illness_list,
+            drug_allergies: this.allergies_list,
+            prophylaxis: this.prophylaxis_list
+        })
     }
-
-    /**
-     * Validation for template driven forms
-     * Visit angular.io -> 
-     * Forms Validation Cookbook Tutorial
-     */
-    patientForm: NgForm;
-    // Queries for the form-> patientForm
-    @ViewChild('patientForm') currentForm: NgForm;
-
-    // Checks for changes to the orm (html template)
-    ngAfterViewChecked() {
-        this.formChanged();
-    }
-
-    formChanged() {
-        if (this.currentForm === this.patientForm) { return; }
-        this.patientForm = this.currentForm;
-        if (this.patientForm) {
-            this.patientForm.valueChanges
-                .subscribe(data => this.onValueChanged(data));
-        }
-    }
-
-    onValueChanged(data?: any) {
-        if (!this.patientForm) { return; }
-        const form = this.patientForm.form;
-        console.log(form);
-        for (const field in this.formErrors) {
-            // clear previous error message (if any)
-            this.formErrors[field] = '';
-            const control = form.get(field);
-            if (control && control.dirty && !control.valid) {
-                const messages = this.validationMessages[field];
-                for (const key in control.errors) {
-                    this.formErrors[field] += messages[key] + ' ';
-                }
-            }
-        }
-    }
-
-    formErrors = {
-        'ccc_number': '',
-        'first_name': '',
-        'weight': '',
-        'height': ''
-    };
-
-    validationMessages = {
-        'ccc_number': {
-            'required': 'ccc number is required.'
-        },
-        'first_name': {
-            'required': `Patients first name is required.`
-        },
-        'weight': {
-            'required': `Patient's weight is required.`
-        },
-        height: {
-            'required': `Patient's height is required.`
-        }
-    };
 
     /**
      * Methods prefixed with set... modify the property values of
      * the patient's patient.
      */
-
-    setDateOfBirth(value: any) {
-        this.patient.birth_date = value; // set the input control date
-        this.patient.getAge(value);
-    }
-
     setDate(value: any, val: string) {
+        if (val == 'birthday') {
+            this.patientForm.patchValue({
+                birth_date: value
+            });
+            this.getAge(value);
+        }
         if (val == 'tb_start') {
-            this.patient.start_tb_phase = value;
+            this.patientForm.patchValue({
+                tb_start: value
+            })
         }
         if (val == 'tb_end') {
-            this.patient.end_tb_phase = value;
+            this.patientForm.patchValue({
+                tb_end: value
+            })
         }
         if (val == 'enrollment') {
-            this.patient.enrollment_date = value;
+            this.patientForm.patchValue({
+                enrollment_date: value
+            })
         }
         if (val == 'regimen') {
-            this.patient.regimen_start_date = value;
+            this.patientForm.patchValue({
+                regimen_start_date: value
+            })
         }
         if (val == 'isoniazid_start') {
-            this.patient.isoniazid_start = value;
+            this.patientForm.patchValue({
+                isoniazid_start: value
+            })
         }
         if (val == 'isoniazid_end') {
-            this.patient.isoniazid_end = value;
+            this.patientForm.patchValue({
+                isoniazid_end: value
+            })
         }
     }
 
-    setEnrollDate(value: any, val: string) {
-        this.patient.enrollment_date = value;
-        console.log(val);
-    }
+    getAge(value: any): any {
+        let dob: any = new Date(value);
+        let today: any = new Date();
+        let age_in_years: number;
+        let age_in_months: number;
 
-    setregimenDate(value: any) {
-        this.patient.regimen_start_date = value;
+        age_in_years = Math.floor((today - dob) / (365.25 * 24 * 60 * 60 * 1000));
+        var y1 = today.getFullYear();
+        var y2 = dob.getFullYear();
+        age_in_months = (today.getMonth() + y1 * 12) - (dob.getMonth() + y2 * 12);
+
+        this.patientForm.patchValue({
+            age_in_years: age_in_years,
+            age_in_months: age_in_months
+        });
     }
 
     setService(value) {
