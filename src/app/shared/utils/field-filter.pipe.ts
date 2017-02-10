263e9bb770aff5c8d0ceb73511ae64641e881d65
bryan-mwas <mwathibrian7@gmail.com>
2017-02-10 13:46:58 +0300

    Contacts and Service Filter

--- src/app/shared/utils/field-filter.pipe.ts
+++ src/app/shared/utils/field-filter.pipe.ts
@@ -4,10 +4,18 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'fieldFilter',
 })
 export class FieldFilterPipe implements PipeTransform {
+  // Compares first parameter to the second one
+  checkValue(p1, p2) {
+    return p1 == p2;
+  }
 
-  transform(items: any[], field : string, value : string): any[] {
+  transform(items: any[], field: string, value: string): any[] {
     if (!items) return [];
     if (!value) return items;
+    if (field == 'service_id' || 'phone_number') {
+      return items.filter(it => this.checkValue(it[field], value));
+      // console.log(items);
+    }
     return items.filter(it => it[field].toLowerCase().indexOf(value.toLowerCase()) > -1);
   }
 }
