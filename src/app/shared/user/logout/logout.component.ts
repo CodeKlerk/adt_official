5e488b7047427e262b21ec4ef33bfd31fe376c63
[anamikoye] <[anamikoye52@gmail.com]>
2017-02-10 12:44:54 +0300

    Ehh

--- src/app/shared/user/logout/logout.component.ts
+++ src/app/shared/user/logout/logout.component.ts
@@ -9,7 +9,7 @@ declare var $:any;
 <div id="logout" (click)="showPopup()" class="btn-header transparent pull-right">
         <span> <a routerlink="/auth/login" title="Sign Out" data-action="userLogout"
                   data-logout-msg="You can improve your security further after logging out by closing this opened browser"><i
-          class="fa fa-sign-out"></i></a> </span>
+          class="fa fa-power-off"></i></a> </span>
     </div>
   `,
   styles: []
